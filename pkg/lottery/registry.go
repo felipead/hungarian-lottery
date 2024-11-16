8c1a64b9d6ead9ae35751abba1e9fd457110a987
Felipe Dornelas <m@felipedornelas.com>
2024-11-16 11:32:39 -0300

    test: more test scenarios

--- pkg/lottery/registry.go
+++ pkg/lottery/registry.go
@@ -11,15 +11,15 @@ type registry struct {
 	// We create several buckets, or bins, one for each possible lottery number.
 	// Assuming the possible lottery numbers are a relatively small set, memory footprint is manageable.
 	//
-	buckets []bucket
+	buckets []bucketType
 }
 
-type bucket = map[PlayerID]bool
+type bucketType = map[PlayerID]bool
 
 func NewRegistry() Registry {
-	buckets := make([]bucket, MaxNumber)
+	buckets := make([]bucketType, MaxNumber)
 	for i := 0; i < MaxNumber; i++ {
-		buckets[i] = make(bucket)
+		buckets[i] = make(bucketType)
 	}
 	return &registry{
 		buckets: buckets,
@@ -28,24 +28,24 @@ func NewRegistry() Registry {
 
 func (r *registry) RegisterPlayerPicks(playerID PlayerID, picks []Number) {
 	for _, number := range picks {
-		r.buckets[number][playerID] = true
+		bucket := r.buckets[number-1]
+		bucket[playerID] = true
 	}
 }
 
 func (r *registry) ProcessLotteryPicks(picks []Number) Report {
 	playerMatches := make(map[PlayerID]int)
 
-	for _, pick := range picks {
-		for playerID := range r.buckets[pick] {
+	for _, number := range picks {
+		bucket := r.buckets[number-1]
+		for playerID := range bucket {
 			playerMatches[playerID] += 1
 		}
 	}
 
 	report := NewReport()
-
 	for _, count := range playerMatches {
 		report.IncrementWinnersOf(count)
 	}
-
 	return report
 }
